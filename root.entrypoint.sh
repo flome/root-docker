cf6068e18478b7bd0755b32e489e424cb5eb4722
Florian Mentzel <florian.mentzel@tu-dortmund.de>
2019-06-05 09:55:57 +0200

    enhance console output for using singularity

--- root.entrypoint.sh
+++ root.entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 source /usr/local/root-6.16.00/bin/thisroot.sh
 
 export XAUTHORITY=/root/.Xauthority 
-exec "$@"
+
+echo " |--- Welcome to the ROOT 6.16.00 Singularity container ---| "
+if [[ ! -z "$@" ]]; then
+    exec "$@"
+fi
+    exec bash --rcfile <(echo "PS1='<Singularity: $SINGULARITY_CONTAINER> \u@\h:\w\$ ' ") -i
